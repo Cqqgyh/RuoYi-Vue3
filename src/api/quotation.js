5d75f1a6b0fce26564c756eef4dd1fe58f0edb02
崔强强 <945327638@qq.com>
2025-11-05 15:09:11 +0800

    feat: 增加报价管理demo

---
+++ src/api/quotation.js
@@ -0,0 +1,58 @@
+import request from '@/utils/request.js'
+import { parseStrEmpty } from "@/utils/ruoyi.js";
+
+// 查询列表
+export function getListPage(query) {
+  return request({
+    url: '/system/quotation/batch/page',
+    method: 'get',
+    params: query
+  })
+}
+// 查询列表
+export function getListPageAll() {
+  return request({
+    url: '/system/quotation/batch/list',
+    method: 'get',
+  })
+}
+// 新增
+export function addRequest(data) {
+  return request({
+    url: '/system/quotation/batch',
+    method: 'post',
+    data: data
+  })
+}
+
+// 查询客户端详细
+export function getDetailRequest(id) {
+  return request({
+    url: `/system/quotation/batch/${parseStrEmpty(id)}`,
+    method: 'get'
+  })
+}
+
+// 修改
+export function updateRequest(data) {
+  return request({
+    url: '/system/quotation/batch',
+    method: 'put',
+    data: data
+  })
+}
+
+// 删除
+export function delRequest(id) {
+  return request({
+    url: `/system/quotation/batch/single/${parseStrEmpty(id)}`,
+    method: 'post'
+  })
+}
+// 批量删除
+export function delBatchRequest(ids) {
+  return request({
+    url: `/system/quotation/batch/${ids}`,
+    method: 'delete'
+  })
+}
