cbcf43b5fa554936026b02069f5f898c484c30c4
崔强强 <945327638@qq.com>
2025-11-17 11:52:04 +0800

    feat: -84清空

--- src/layout/components/AppMain.vue
+++ src/layout/components/AppMain.vue
@@ -62,13 +62,13 @@ function addIframe() {
 .hasTagsView {
   .app-main {
     /* 84 = navbar + tags-view = 50 + 34 */
-    min-height: calc(100vh - 84px);
+    //min-height: calc(100vh - 84px);
     //min-height: calc(100vh);
   }
 
   .fixed-header + .app-main {
     //margin-top: 84px;
-    height: calc(100vh - 84px);
+    //height: calc(100vh - 84px);
     //height: calc(100vh);
     min-height: 0px;
   }
